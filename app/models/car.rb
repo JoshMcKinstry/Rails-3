5220c57f1235708874a91ba3bef5651fe1e50d86
Josh McKinstry <jbmckinstry@gmail.com>
2020-08-03 12:48:06 -0600

    car attributes presence required

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,4 +2,5 @@ class Car < ApplicationRecord
     belongs_to :make
     has_many :car_parts
     has_many :parts, through: :car_parts
+    validates :vin, :model, presence: true
 end
--- test/models/car_test.rb
+++ test/models/car_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class CarTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "invalid without make" do
+    #belongs_to association creates a validation by default
+    c = cars(:nomake)
+    assert c.invalid?, "Car without make should be invalid"
+    refute c.save
+    assert_not_nil c.errors[:make]
+  end
+
+  test "invalid without VIN" do
+    c = cars(:novin)
+    assert c.invalid?, "Car without VIN should be invalid"
+    refute c.save
+    assert_not_nil c.errors[:vin]
+  end
+
+  test "invalid without model" do
+    c = cars(:nomodel)
+    assert c.invalid?, "Car without model should be invalid"
+    refute c.save
+    assert_not_nil c.errors[:model]
+  end
+
+  test "VIN is length 17" do
+    c = cars(:one)
+    assert_equal 17, c.vin.size, "VIN should be 17 characters long"
+    assert_nil c.errors.messages
+  end
 end
