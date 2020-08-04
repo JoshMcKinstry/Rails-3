d9f4379238da91e0fce6b0bc6af012b32bb8f2f1
Josh McKinstry <jbmckinstry@gmail.com>
2020-08-03 23:47:40 -0600

    some make model tests and placeholders

--- app/models/make.rb
+++ app/models/make.rb
@@ -1,3 +1,4 @@
 class Make < ApplicationRecord
     has_many :cars
+    validates :name, :country, presence: true
 end
--- test/models/make_test.rb
+++ test/models/make_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class MakeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "missing name" do
+    make = Make.create(country: "Germany")
+    assert make.invalid?
+    refute make.save
+  end
+
+  test "missing country" do
+    make = Make.create(name: "Honda")
+    assert make.invalid?
+    refute make.save
+  end
+
+  test "attibutes present" do
+    make = Make.create(name: "Honda", country: "Japan"))
+    assert make.valid?
+    assert make.save
+  end
+
+  test "name must not be similar" do
+  end
+
+  test "unique name is valid" do
+  end
+
+  test "name too long" do
+  end
+
+  test "name is valid length" do
+  end
+
 end
