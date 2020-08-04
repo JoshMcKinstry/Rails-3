2a853050eb8a32d4ff98a4acb546dbe799443ea9
Josh McKinstry <jbmckinstry@gmail.com>
2020-08-03 19:59:56 -0600

    vin is length 10, unique

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,4 +3,5 @@ class Car < ApplicationRecord
     has_many :car_parts
     has_many :parts, through: :car_parts
     validates :vin, :model, presence: true
+    validates :vin, length: {is: 10}, uniqueness: true
 end
--- test/models/car_test.rb
+++ test/models/car_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class CarTest < ActiveSupport::TestCase
   test "invalid without make" do
     #belongs_to association creates a validation by default
-    c = cars(:nomake)
+    c = Car.create({:model => "Model", :vin => "12345ABCDE"})
     assert c.invalid?, "Car without make should be invalid"
     refute c.save
     assert_not_nil c.errors[:make]
@@ -23,9 +23,30 @@ class CarTest < ActiveSupport::TestCase
     assert_not_nil c.errors[:model]
   end
 
-  test "VIN is length 17" do
+  # Real VIN are 17 digits, here they will be 10
+  test "VIN is length 10" do
     c = cars(:one)
-    assert_equal 17, c.vin.size, "VIN should be 17 characters long"
-    assert_nil c.errors.messages
+    assert c.valid?
+    assert_equal 10, c.vin.size, "VIN should be 10 characters long"
+    assert_equal({}, c.errors.messages)
+  end
+
+  test "VIN cannot be less than 10" do
+    c = cars(:tooshort)
+    assert c.invalid?, "VIN can only be 10 characters"
+    assert_not_empty c.errors[:vin]
+  end
+
+  test "VIN cannot be greater than 10" do
+    c = cars(:toolong)
+    assert c.invalid?, "VIN can only be 10 characters"
+    assert_not_empty c.errors[:vin]
+  end
+
+  test "VIN must be unique" do
+    c = Car.create({:make_id => 1, :model => "Model", :vin => "11AA22BB33"})
+    assert c.invalid?, "VIN must be unique"
+    refute c.save
+    assert_not_empty c.errors[:vin]
   end
 end
