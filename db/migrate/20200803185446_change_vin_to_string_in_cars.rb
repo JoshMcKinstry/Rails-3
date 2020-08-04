85078c218ebbde21d6640d0edab39fd8cf531bc1
Josh McKinstry <jbmckinstry@gmail.com>
2020-08-03 19:04:03 -0600

    referencing source for fix

--- db/migrate/20200803185446_change_vin_to_string_in_cars.rb
+++ db/migrate/20200803185446_change_vin_to_string_in_cars.rb
@@ -1,4 +1,5 @@
 class ChangeVinToStringInCars < ActiveRecord::Migration[5.2]
+  #https://stackoverflow.com/questions/51814399/when-renaming-column-in-users-table-getting-sqlite3constraintexception-forei/51815720#51815720
   def change
     connection.execute("PRAGMA defer_foreign_keys = ON")
     connection.execute("PRAGMA foreign_keys = OFF")
