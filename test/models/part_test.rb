e6019aea80c8cb7a292df4b4c49b8de48375388c
Josh McKinstry <jbmckinstry@gmail.com>
2020-08-03 23:55:01 -0600

    more placeholder tests for part

--- test/models/part_test.rb
+++ test/models/part_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class PartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "missing part name" do
+  end
+
+  test "name present" do
+  end
+
+  test "part names must not be similar" do
+  end
+
+  test "unique part name is valid" do
+  end
+
+  test "part name too long" do
+  end
+
+  test "part name length is valid" do
+  end
 end
