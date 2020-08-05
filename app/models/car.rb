b8b3b69695603ed16be43d7c35ea93f103164504
Josh McKinstry <jbmckinstry@gmail.com>
2020-08-04 21:09:27 -0600

    destroy hanging car_parts links when a car is destroyed

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,6 @@
 class Car < ApplicationRecord
     belongs_to :make
-    has_many :car_parts
+    has_many :car_parts, dependent: :destroy
     has_many :parts, through: :car_parts
     validates :model, presence: true, length: {in: 2..20}
     validates :vin, presence: true, length: {is: 10}, uniqueness: true
