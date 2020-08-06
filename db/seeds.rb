f2954f38043d97a9cc45e92c0ff409d6e3e9da7b
Josh McKinstry <jbmckinstry@gmail.com>
2020-08-05 20:49:50 -0600

    part and make data, seed dump

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,53 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
+Car.destroy_all
+Make.destroy_all
+Part.destroy_all
+CarPart.destroy_all
+
+Part.create([
+    {name: "Engine"},
+    {name: "Battery"},
+    {name: "Rear Bumper"},
+    {name: "Front Bumper"},
+    {name: "Distributor"},
+    {name: "Headlight"},
+    {name: "Airbag"},
+    {name: "Spark Plug"},
+    {name: "Starter"},
+    {name: "Dashboard"},
+    {name: "Front Seat"},
+    {name: "Fan Belt"},
+    {name: "Oil Filter"},
+    {name: "Air Filter"},
+    {name: "Fuel Tank"},
+    {name: "Shock Absorber"},
+    {name: "Brakes"}
+])
+
+Make.create([
+    {name: "Cadillac", country: "United States"},
+    {name: "Ford", country: "United States"},
+    {name: "Tesla", country: "United States"},
+    {name: "Chevrolet", country: "United States"},
+    {name: "Nissan", country: "Japan"},
+    {name: "Lexus", country: "Japan"},
+    {name: "Subaru", country: "Japan"}, #This is Colorado, I had to include Subaru
+    {name: "Toyota", country: "Japan"},
+    {name: "Hyundai", country: "South Korea"},
+    {name: "Kia", country: "South Korea"},
+    {name: "Audi", country: "Germany"},
+    {name: "Mercedes-Benz", country: "Germany"},
+    {name: "Porsche", country: "Germany"},
+    {name: "Volkswagen", country: "Germany"},
+    {name: "Ferrari", country: "Italy"},
+    {name: "Lamborghini", country: "Italy"},
+    {name: "Maserati", country: "Italy"},
+    {name: "Volvo", country: "Sweden"},
+    {name: "Aston Martin", country: "United Kingdom"},
+    {name: "Bentley", country: "United Kingdom"},
+    {name: "Jaguar", country: "United Kingdom"},
+    {name: "McLaren", country: "United Kingdom"}
+])
+
+
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
